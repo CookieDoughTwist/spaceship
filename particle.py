df38c2f3c137381e491e519d7884d0afd8ceb96e
CookieDoughTwist <andrew01810@gmail.com>
2016-10-12 20:30:21 -0400

    Fixed flare animation bug.

--- particle.py
+++ particle.py
@@ -112,7 +112,7 @@ class flaming_falcon(object):
 		self.rcs_thrust = 1e5
 		self.init_step = init_step
 		self.current_step = init_step
-		self.fire_cd = 1.0 # seconds
+		self.fire_cd = 0.5 # seconds
 		self.fire_last = -float('inf')
 		
 		self.state = baryonic_state()
@@ -302,7 +302,7 @@ class flaming_falcon(object):
 		# Paint main gun
 		fire_period = 0.25 # seconds
 		fire_progress = min((self.current_step-self.fire_last)/(fire_period*60),1.0)
-		if fire_progress == 0:
+		if fire_progress <= .1:
 			image.blit(self.bullet_flare,(241,89))
 		recoil_coef = math.sin(fire_progress*math.pi)
 		image.blit(self.gun_image,(247,115+15*recoil_coef)) # y = 115 is home position
