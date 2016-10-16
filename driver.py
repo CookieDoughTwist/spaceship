666b548a084b221cdd1adeb27047036623ce4b4f
CookieDoughTwist <andrew01810@gmail.com>
2016-10-16 02:06:57 -0400

    Refactored driver.py into a class structure.

--- driver.py
+++ driver.py
@@ -3,117 +3,131 @@ import math
 import particle
 import operations
 import spaceship
-
-
-def display_particle(screen,screen_origin,particle):
-	state = particle.state
-	x_m = state.x # meters
-	y_m = state.y # meters
-	x = x_m*5
-	y = y_m*5
-	image = particle.get_image()
-	center = image.get_rect().center
-	x_bit = x-center[0]-screen_origin[0]
-	y_bit = y-center[1]-screen_origin[1]	
-	rect = image.get_rect()	
 	
-	# Do not paint things out of bounds
-	if x_bit+rect[2] >= 0 and \
-	   x_bit <= screen.get_width() and \
-	   y_bit+rect[3] >= 0 and \
-	   y_bit <= screen.get_height():
-		screen.blit(image,(x_bit,y_bit))	
-
-	
-def paint_engine(screen,screen_origin,engine):
-	for entity in engine.entities:
-		display_particle(screen,screen_origin,entity)
-	
-def paint_grid(screen,screen_origin,grid_width):
-	w = screen.get_width()
-	h = screen.get_height()
-	x_offset = -(screen_origin[0] % grid_width)+grid_width
-	y_offset = -(screen_origin[1] % grid_width)+grid_width
-	for x in range(x_offset,w+x_offset,grid_width):
-		vertical_line = pygame.Surface((1, h), pygame.SRCALPHA)
-		vertical_line.fill((0, 255, 0, 30))
-		screen.blit(vertical_line, (x - 1, 0))
-	for y in range(y_offset,h+y_offset,grid_width):
-		horizontal_line = pygame.Surface((w, 1), pygame.SRCALPHA)
-		horizontal_line.fill((0, 255, 0, 30))
-		screen.blit(horizontal_line, (0, y - 1))
-
-def paint_dot(screen,screen_origin,pos):
-	pygame.draw.circle(screen,(255,0,0),(pos[0]-screen_origin[0],pos[1]-screen_origin[1]),10)
-	
-def main():
-	pygame.init()
-	screen_origin = [-27,-23]
-	lock_screen = False
-	grid_width = 500 # bits	
-	dis_info = pygame.display.Info()	
-	#screen = pygame.display.set_mode((dis_info.current_w, dis_info.current_h))
-	screen = pygame.display.set_mode((dis_info.current_w, dis_info.current_h),pygame.FULLSCREEN)
-	done = False
-	clock = pygame.time.Clock()
-	frame_rate = 60 # Hz
-	
-	engine = spaceship.engine(pygame.time.get_ticks(),frame_rate)
+class pygame_wrapper(object):
+	""" Holds pygame engine and display parameters """
+	def __init__(self):
+		pygame.init()
+		dis_info = pygame.display.Info()
+		self.screen_dim = [dis_info.current_w, dis_info.current_h]
+		self.screen_center = [self.screen_dim[0]/2,self.screen_dim[1]/2]
+		self.screen = pygame.display.set_mode(\
+			self.screen_dim,pygame.FULLSCREEN)
+		self.clock = pygame.time.Clock()
+		self.frame_rate = 60 # Hz
+		self.bits_per_meter = 5
+		self.lock_screen = False
+		self.grid_width = 100 # meters
 		
-	engine.place_hyper_neutron((310,103))
+		self.engine = spaceship.engine(\
+			pygame.time.get_ticks(),self.frame_rate)
+			
+		self.stop = False
 		
-	while not done: 
-	
+	def step(self):
 		pressed = pygame.key.get_pressed()
 		alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
 		ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
 		
 		event_list = pygame.event.get()	
+		
 		for event in event_list:
 			if event.type == pygame.QUIT:
-				done = True
+				self.stop = True
+				return
 			# Closing Strokes
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_w and ctrl_held:
+					self.stop = True
 					return
 				if event.key == pygame.K_F4 and alt_held:
-					return	
+					self.stop = True
+					return
 				if event.key == pygame.K_y:
-					lock_screen = not lock_screen								
+					self.lock_screen = not self.lock_screen								
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if event.button == 1: # left click grows radius 
 					print 'cheese'
 				elif event.button == 3: # right click shrinks radius 
 					print 'balls'
 		
-		if lock_screen:
-			w = screen.get_width()
-			h = screen.get_height()
-			x_m = engine.focus.state.x
-			y_m = engine.focus.state.y
-			x = int(x_m*5-w/2)
-			y = int(y_m*5-h/2)
-			screen_origin = [x,y]
+		if self.lock_screen:
+			x_m = self.engine.focus.state.x
+			y_m = self.engine.focus.state.y
+			x = int(x_m*self.bits_per_meter)
+			y = int(y_m*self.bits_per_meter)
+			self.screen_center = [x,y]
 		else:
 			if pressed[pygame.K_UP]:
-				screen_origin[1] -= 10
+				self.screen_center[1] -= 10
 			if pressed[pygame.K_DOWN]:
-				screen_origin[1] += 10
+				self.screen_center[1] += 10
 			if pressed[pygame.K_LEFT]:
-				screen_origin[0] -= 10
+				self.screen_center[0] -= 10
 			if pressed[pygame.K_RIGHT]:
-				screen_origin[0] += 10
+				self.screen_center[0] += 10
 		
 
 		
-		engine.step(pygame.time.get_ticks(),pressed,event_list)
+		self.engine.step(pygame.time.get_ticks(),pressed,event_list)
 		
-		screen.fill((30,30,30))
-		paint_grid(screen,screen_origin,grid_width)
-		paint_dot(screen,screen_origin,(0,0))
-		paint_engine(screen,screen_origin,engine)
+		self.screen.fill((30,30,30))
+		self.paint_grid()
+		self.paint_dot((0,0))
+		self.paint_engine()
 		pygame.display.flip()
-		clock.tick(frame_rate)
+		self.clock.tick(self.frame_rate)
 		
+	def paint_grid(self):
+		bit_width = self.grid_width*self.bits_per_meter
 		
+		screen_origin = self.get_screen_origin()
+		x_offset = -(screen_origin[0] % bit_width)+bit_width
+		y_offset = -(screen_origin[1] % bit_width)+bit_width
+		
+		w = self.screen_dim[0]
+		h = self.screen_dim[1]
+		
+		for x in range(x_offset,w+x_offset,bit_width):			
+			vertical_line = pygame.Surface((1, h), pygame.SRCALPHA)
+			vertical_line.fill((0, 255, 0, 30))
+			self.screen.blit(vertical_line, (x - 1, 0))
+		for y in range(y_offset,h+y_offset,bit_width):
+			horizontal_line = pygame.Surface((w, 1), pygame.SRCALPHA)
+			horizontal_line.fill((0, 255, 0, 30))
+			self.screen.blit(horizontal_line, (0, y - 1))
+			
+	def paint_engine(self):
+		screen_origin = self.get_screen_origin()
+		for entity in self.engine.entities:
+			state = entity.state			
+			x = state.x*self.bits_per_meter
+			y = state.y*self.bits_per_meter
+			image = entity.get_image()
+			center = image.get_rect().center
+			x_bit = x-center[0]-screen_origin[0]
+			y_bit = y-center[1]-screen_origin[1]	
+			rect = image.get_rect()	
+			
+			# Do not paint things out of bounds
+			if x_bit+rect[2] >= 0 and \
+			   x_bit <= self.screen.get_width() and \
+			   y_bit+rect[3] >= 0 and \
+			   y_bit <= self.screen.get_height():
+				self.screen.blit(image,(x_bit,y_bit))
+			
+	def paint_dot(self,pos):
+		screen_origin = self.get_screen_origin()
+		pygame.draw.circle(self.screen,(255,0,0),(pos[0]-screen_origin[0],pos[1]-screen_origin[1]),10)
+
+	def get_screen_origin(self):
+		return (self.screen_center[0]-self.screen_dim[0]/2,\
+				self.screen_center[1]-self.screen_dim[1]/2)
+	
+def main():
+	driver = pygame_wrapper()
+	driver.engine.place_hyper_neutron((310,103))
+	while not driver.stop:
+		driver.step()
+
 main()
--- operations.py
+++ operations.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import numpy as np
 
 def rot_center(image, angle):
     """rotate an image while keeping its center and size"""
@@ -9,3 +10,8 @@ def rot_center(image, angle):
     rot_rect.center = rot_image.get_rect().center
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
+	
+def rot_matrix(theta):
+	""" clockwise angle rotation matrix """
+	return np.matrix((( math.cos(theta),math.sin(theta)),
+			          (-math.sin(theta),math.cos(theta))))
